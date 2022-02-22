a7bb4a43b952bf04dcc9caea7da74d7549da6344
winchell459 <johncmorris@gmail.com>
2022-02-22 11:58:36 -0800

    how to play added

--- Assets/ASPSampleScene/Scripts/Golf/MainMenu.cs
+++ Assets/ASPSampleScene/Scripts/Golf/MainMenu.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
+    bool howToPlaying = false;
+    public GameObject HowToPlayPanel;
     
     public void StartGerrymanderingButton()
     {
@@ -14,6 +16,21 @@ public class MainMenu : MonoBehaviour
     private void loadScene(string sceneName)
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+    }
+
+    public void HowToPlayButton()
+    {
+        HowToPlayPanel.SetActive(true);
+        howToPlaying = true;
+    }
+
+    private void OnMouseDown()
+    {
+        if (howToPlaying)
+        {
+            HowToPlayPanel.SetActive(false);
+            howToPlaying = false;
+        }
     }
 }
 
